bf66384399ad82bdf42d3ac8896839c06500d11f
zollahungry <zollahungry@naver.com>
2017-12-02 01:42:53 +0900

    버튼 정렬 추가 인트로화면 싱글 멀티 랭킹 버튼 추가 게임매니저 추가 버튼에 따라 정보를 받음

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    Debug.LogWarning(string.Format("{0} 인스턴스를 찾을 수 없습니다.", typeof(GameManager).ToString()));
+                }
+            }
+            return _instance;
+        }
+    }
+
+
+
+    public int gameMode = 0;
+    public int player_1 = 0;
+    public int player_2 = 0;
+    public int map = 0;
+
+    private void OnEnable()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -6,11 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class IntroManager : MonoBehaviour {
 
-    const int IntroButtonMax = 2;
-    const int CharButtonMax = 4;
+    public GameManager _gameManager;
+    public GameManager GameManager
+    {
+        get
+        {
+            if (_gameManager == null)
+                _gameManager = FindObjectOfType<GameManager>();
 
-    private bool axisInUse = false;
-    int selectedButton = 0;
+            return _gameManager;
+        }
+    }
+
+    const int IntroButtonMax = 4;
+    const int CharButtonMax = 4;
+    const int MapButtonMax = 4;
+
+    enum Mode { single = 0, multi = 1 }
+    enum Character { matilda = 0, captain = 1, navi = 2, terminator = 3 }
+    enum Map { apocalypse = 0, city = 1, militery = 2, port = 3 }
+
+    private bool axisInUse_1 = false;
+    private bool axisInUse_2 = false;
+
+    int selectedButton_1 = 0;
+    int selectedButton_2 = 1;
     int thisSceneButtonMax = 0;
 
     List<Image> buttonList;
@@ -18,70 +38,169 @@ public class IntroManager : MonoBehaviour {
     private void Start()
     {
         SelectButtonByScene(SceneManager.GetActiveScene().buildIndex);
-        SelectButton(selectedButton);
+        SelectButton_1(selectedButton_1);
     }
 
     private void Update()
     {
-        UserInputs();
+        UserInputs_1();
+        UserInputs_2();
+        ButtonEffect();
     }
 
 
-    void UserInputs()
+    void UserInputs_1()
     {
         if (Input.GetAxisRaw("Horizontal") == 1 || (Input.GetAxisRaw("1P_Triggers") == 1) || (Input.GetAxisRaw("1P_HorizontalDPad") == 1))
         {
-            if (axisInUse == false)
+            if (axisInUse_1 == false)
             {
-                axisInUse = true;
+                axisInUse_1 = true;
 
-                selectedButton++;
+                selectedButton_1++;
 
-                if (selectedButton == thisSceneButtonMax)
-                    selectedButton--;
+                if (selectedButton_1 == thisSceneButtonMax)
+                    selectedButton_1--;
 
-                SelectButton(selectedButton);
+                SelectButton_1(selectedButton_1);
                 StartCoroutine(ButtonSelectDelay());
             }
         }
         else if (Input.GetAxisRaw("Horizontal") == -1 || (Input.GetAxisRaw("1P_Triggers") == -1) || (Input.GetAxisRaw("1P_HorizontalDPad") == -1))
         {
-            if (axisInUse == false)
+            if (axisInUse_1 == false)
             {
-                axisInUse = true;
+                axisInUse_1 = true;
 
-                selectedButton--;
+                selectedButton_1--;
 
-                if (selectedButton < 0)
-                    selectedButton++;
+                if (selectedButton_1 < 0)
+                    selectedButton_1++;
 
-                SelectButton(selectedButton);
+                SelectButton_1(selectedButton_1);
                 StartCoroutine(ButtonSelectDelay());
             }
         }
 
+    
         if ((Input.GetButtonDown("1P_XBtn") || Input.GetKeyDown(KeyCode.Space)))
         {
             switch (SceneManager.GetActiveScene().buildIndex)
             {
-                case 0:
-                    //Intro씬일 때
-                    if (selectedButton == 0)
+                case 0: //Intro씬일 때
+                    if (selectedButton_1 == 0)//Single
+                    {
+                        GameManager.Instance.gameMode = (int)Mode.single;
                         SceneManager.LoadScene(1);
+                        Debug.Log(GameManager.Instance.gameMode);
+                    }
+                    if (selectedButton_1 == 1)//Multi
+                    {
+                        GameManager.Instance.gameMode = (int)Mode.multi;
+                        //2P 활성화
+                        SceneManager.LoadScene(1);
+                    }
+                    if (selectedButton_1 == 2)//Ranking
+                    {
+                        //랭킹씬으로
+                    } 
                     else
                         Application.Quit();
                     break;
 
-                case 1:
-                    //CharacterSelect씬일 때
-                    //캐릭터 선택으로 수정해야함
-                    if (selectedButton == 0)
-                        SceneManager.LoadScene(2);
+                case 1: //CharacterSelect씬일 때
+
+                    if (GameManager.Instance.gameMode == (int)Mode.single)
+                    {
+                        if (selectedButton_1 == 0)
+                        {
+                            GameManager.Instance.player_1 = (int)Character.matilda;
+                            SceneManager.LoadScene(2);
+                        }
+                        if (selectedButton_1 == 1)
+                        {
+                            GameManager.Instance.player_1 = (int)Character.captain;
+                            SceneManager.LoadScene(2);
+                        }
+                        if (selectedButton_1 == 2)
+                        {
+                            GameManager.Instance.player_1 = (int)Character.navi;
+                            SceneManager.LoadScene(2);
+                        }
+                        if (selectedButton_1 == 3)
+                        {
+                            GameManager.Instance.player_1 = (int)Character.terminator;
+                            SceneManager.LoadScene(2);
+                        }
+                    }
+                    break;
+                    
+                    case 2: //MapSelect씬일 때
+
+                    if (GameManager.Instance.gameMode == (int)Mode.single)
+                    {
+                        if (selectedButton_1 == 0)
+                        {
+                            GameManager.Instance.map = (int)Map.apocalypse;
+                            SceneManager.LoadScene(3);
+                            Debug.Log(GameManager.Instance.gameMode);
+                            Debug.Log(GameManager.Instance.player_1);
+                            Debug.Log(GameManager.Instance.map);
+                        }
+                        if (selectedButton_1 == 1)
+                        {
+                            GameManager.Instance.map = (int)Map.city;
+                            SceneManager.LoadScene(4);
+                        }
+                        if (selectedButton_1 == 2)
+                        {
+                            GameManager.Instance.map = (int)Map.militery;
+                            SceneManager.LoadScene(5);
+                        }
+                        if (selectedButton_1 == 3)
+                        {
+                            GameManager.Instance.map = (int)Map.port;
+                            SceneManager.LoadScene(6);
+                        }
+                    }
                     break;
             }
         }
     }
     
+    void UserInputs_2()
+    {
+        if (Input.GetAxisRaw("Vertical") == 1 || (Input.GetAxisRaw("1P_Triggers") == 1) || (Input.GetAxisRaw("1P_HorizontalDPad") == 1))
+        {
+            if (axisInUse_2 == false)
+            {
+                axisInUse_2 = true;
+
+                selectedButton_2++;
+
+                if (selectedButton_2 == thisSceneButtonMax)
+                    selectedButton_2--;
+
+                SelectButton_2(selectedButton_2);
+                StartCoroutine(ButtonSelectDelay2());
+            }
+        }
+        else if (Input.GetAxisRaw("Vertical") == -1 || (Input.GetAxisRaw("1P_Triggers") == -1) || (Input.GetAxisRaw("1P_HorizontalDPad") == -1))
+        {
+            if (axisInUse_2 == false)
+            {
+                axisInUse_2 = true;
+
+                selectedButton_2--;
+
+                if (selectedButton_2 < 0)
+                    selectedButton_2++;
+
+                SelectButton_2(selectedButton_2);
+                StartCoroutine(ButtonSelectDelay2());
+            }
+        }
+    }
 
     void SelectButtonByScene(int sceneNumber)
     {
@@ -108,11 +227,39 @@ public class IntroManager : MonoBehaviour {
                     buttonList.Add(button);
                 }
                 break;
+            case 2:
+                //MapSelect씬일 때
+                thisSceneButtonMax = MapButtonMax;
+
+                for (int i = 0; i < MapButtonMax; i++)
+                {
+                    Image button = GameObject.Find("Button" + i).GetComponent<Image>();
+                    buttonList.Add(button);
+                }
+                break;
+        }
+    }
+    void ButtonEffect()
+    {
+        for (int i = 0; i < thisSceneButtonMax; i++)
+        {
+           
+            buttonList[i].color = Color.white;
+            if(i == selectedButton_1)
+            {
+                buttonList[i].color = Color.red;
+            }
+            if (i == selectedButton_2)
+            {
+                buttonList[i].color = Color.blue;
+            }
         }
     }
 
-    void SelectButton(int selectedButtonNumber)
+    void SelectButton_1(int selectedButtonNumber)
     {
+        return;
+
         for (int i = 0; i < thisSceneButtonMax; i++)
         {
             buttonList[i].color = Color.white;
@@ -120,9 +267,26 @@ public class IntroManager : MonoBehaviour {
         buttonList[selectedButtonNumber].color = Color.red;
     }
 
+    void SelectButton_2(int selectedButtonNumber)
+    {
+        return;
+
+        for (int i = 0; i < thisSceneButtonMax; i++)
+        {
+            buttonList[i].color = Color.white;
+        }
+        buttonList[selectedButtonNumber].color = Color.blue;
+    }
+
+
     IEnumerator ButtonSelectDelay()
     {
         yield return new WaitForSeconds(0.2f);
-        axisInUse = false;
+        axisInUse_1 = false;
+    }
+    IEnumerator ButtonSelectDelay2()
+    {
+        yield return new WaitForSeconds(0.2f);
+        axisInUse_2 = false;
     }
 }
